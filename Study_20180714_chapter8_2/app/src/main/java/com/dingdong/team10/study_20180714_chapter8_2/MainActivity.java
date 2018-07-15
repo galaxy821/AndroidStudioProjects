b09433488f87dfe9b441886242e3a972f77843e5
galaxy821 <hoshino208@naver.com>
2018-07-15 15:39:11 +0900

    파일추가

---
+++ Study_20180714_chapter8_2/app/src/main/java/com/dingdong/team10/study_20180714_chapter8_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dingdong.team10.study_20180714_chapter8_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- Study_21080713_chapter7_4/app/src/main/java/com/example/galax/study_21080713_chapter7_4/MainActivity.java
+++ Study_21080713_chapter7_4/app/src/main/java/com/example/galax/study_21080713_chapter7_4/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.galax.study_21080713_chapter7_4;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    ProgressBar progress;
+    BackgroundTask task;
+    int value;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button executeButton =(Button) findViewById(R.id.executeButton);
+        Button cancelButton =(Button) findViewById(R.id.cancelButton);
+        progress = (ProgressBar) findViewById(R.id.progressBar);
+        textView = (TextView) findViewById(R.id.textView);
+
+        executeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task = new BackgroundTask();
+                task.execute(100);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.cancel(true);
+            }
+        });
+    }
+
+    class BackgroundTask extends AsyncTask<Integer, Integer, Integer>{
+        @Override
+        protected void onPreExecute() {
+            value=0;
+            progress.setProgress(value);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+
+            while(isCancelled()==false){
+                value++;
+                if(value>=100){
+                    break;
+                }else{
+                    publishProgress(value);
+                }
+
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException ex){ }
+            }
+            return value;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            progress.setProgress(0);
+            textView.setText("Finished");
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progress.setProgress(values[0].intValue());
+            textView.setText("Current Value : "+ values[0].toString());
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            progress.setProgress(0);
+            textView.setText("Cancelled");
+        }
     }
 }
